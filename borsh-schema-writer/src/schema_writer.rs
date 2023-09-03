f4062d3b13e109459490da8939db8b85cb9729ff
Will Kennedy <will.n.kennedy@gmail.com>
2023-09-03 09:27:57 -0400

    Added schema to bytes function, tests

--- borsh-schema-writer/src/schema_writer.rs
+++ borsh-schema-writer/src/schema_writer.rs
@@ -4,6 +4,15 @@ use borsh::{BorshSchema, BorshSerialize};
 use borsh::schema::BorshSchemaContainer;
 
 /// This function takes a Struct with the BorshSchema trait and writes the schema to a specified file.
+///```rust
+///fn write_schema_test() {
+///     let _ = write_schema(Person::default(), "./tests/schema/person_schema.dat".to_string());
+///
+///     let file = File::open("./tests/schema/person_schema.dat").unwrap();
+///     let mut reader = BufReader::new(file);
+///     let container_from_file = BorshSchemaContainer::deserialize_reader(&mut reader).expect("Deserialization for BorshSchemaContainer failed");
+///}
+/// ```
 pub fn write_schema<T: BorshSchema>(_: T, file_path: String) -> std::io::Result<()> {
     let mut defs = Default::default();
     T::add_definitions_recursively(&mut defs);
@@ -14,4 +23,22 @@ pub fn write_schema<T: BorshSchema>(_: T, file_path: String) -> std::io::Result<
     let mut file = File::create(file_path).expect("Failed to create borsh schema file");
     file.write_all(&data).expect("Failed to write file");
     Ok(())
+}
+
+/// This function takes a Struct with the BorshSchema trait and writes the schema to a specified file.
+///```rust
+///fn write_to_bytes_test() {
+///    let schema = schema_to_bytes(Person::default()).expect("Failed to serialize BorshSchemaContainer");
+///
+///    let container_from_bytes = BorshSchemaContainer::deserialize(&mut schema.as_slice()).expect("Deserialization for BorshSchemaContainer failed");
+///}
+/// ```
+pub fn schema_to_bytes<T: BorshSchema>(_: T) -> std::io::Result<Vec<u8>> {
+    let mut defs = Default::default();
+    T::add_definitions_recursively(&mut defs);
+    let container: BorshSchemaContainer = T::schema_container();
+    let data = container
+        .try_to_vec()
+        .expect("Failed to serialize BorshSchemaContainer");
+    Ok(data)
 }
--- borsh-schema-writer/tests/integration_tests.rs
+++ borsh-schema-writer/tests/integration_tests.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::BufReader;
 use borsh::{BorshDeserialize, BorshSerialize, BorshSchema};
 use borsh::schema::{BorshSchemaContainer, Definition, Fields};
-use borsh_schema_writer::schema_writer::{write_schema};
+use borsh_schema_writer::schema_writer::{schema_to_bytes, write_schema};
 
 #[derive(Debug, Default, BorshSerialize, BorshDeserialize, BorshSchema)]
 pub struct Person {
@@ -20,6 +20,30 @@ fn write_schema_test() {
 
     assert_eq!(container_from_file.declaration.to_string(), "Person");
     let definition = container_from_file.definitions.get(container_from_file.declaration.as_str()).unwrap();
+    assert!(matches!(definition, Definition::Struct { .. }));
+
+    match definition {
+        Definition::Struct { fields } => match fields {
+            Fields::NamedFields(fields) => {
+                for (key, value_declaration) in fields {
+                    assert!(key.as_str() == "first_name" || key.as_str() == "last_name");
+                    assert!(value_declaration.as_str() == "string");
+                }
+            }
+            _ => {assert!(false)}
+        },
+        _ => {assert!(false)}
+    }
+}
+
+#[test]
+fn write_to_bytes_test() {
+    let schema = schema_to_bytes(Person::default()).expect("Failed to serialize BorshSchemaContainer");
+
+    let container_from_bytes = BorshSchemaContainer::deserialize(&mut schema.as_slice()).expect("Deserialization for BorshSchemaContainer failed");
+
+    assert_eq!(container_from_bytes.declaration.to_string(), "Person");
+    let definition = container_from_bytes.definitions.get(container_from_bytes.declaration.as_str()).unwrap();
     assert!(matches!(definition, Definition::Struct { .. }));
 
     match definition {
